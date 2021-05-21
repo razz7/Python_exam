96178cdc591bbdf1c176f13a88b2b99b592b4ce7
Claes <claesvonh@gmail.com>
2021-05-21 21:10:16 +0200

    Modules dir with cleaners.py and notebook Cleaning added

---
+++ notebooks/modules/cleaners.py
@@ -0,0 +1,19 @@
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import string
+import pandas as pd
+
+
+def clean_text(speech):
+    tokens = word_tokenize(speech)
+    # convert to lower case
+    tokens = [w.lower() for w in tokens]
+    # remove punctuation from each word
+    table = str.maketrans('', '', string.punctuation)
+    stripped = [w.translate(table) for w in tokens]
+    # remove remaining tokens that are not alphabetic
+    words = [word for word in stripped if word.isalpha()]
+    # filter out stop words
+    stop_words = set(stopwords.words('english'))
+    words = [w for w in words if not w in stop_words]
+    return words
