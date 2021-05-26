ea0ed5e2da8586b9b4d2df23a567dccfa73d3c74
Claes <claesvonh@gmail.com>
2021-05-26 09:18:09 +0200

    Methods added to cleaning.ipynb

--- notebooks/modules/cleaners.py
+++ notebooks/modules/cleaners.py
@@ -2,6 +2,13 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import string
 import pandas as pd
+import re
+
+
+def simple_clean(text):
+    text = text.lower()
+    text: str = re.sub("\n", "", text)
+    return text
 
 
 def clean_text(speech):
@@ -17,3 +24,18 @@ def clean_text(speech):
     stop_words = set(stopwords.words('english'))
     words = [w for w in words if not w in stop_words]
     return words
+
+
+def clean_two(list):
+    result = []
+    for e in list:
+        tokens = word_tokenize(e)
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        words = [word for word in stripped if word.isalpha()]
+        # filter out stop words
+        from nltk.corpus import stopwords
+        stop_words = set(stopwords.words('english'))
+        words = [w for w in words if not w in stop_words]
+        result.append(words)
+    return result
