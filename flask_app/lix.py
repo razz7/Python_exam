796ecafbd20f54bea5d9031d142ed4479581c730
rasmus h <cph-rh178@cphbusiness.dk>
2021-05-28 15:17:06 +0200

    2 lix charts added

---
+++ flask_app/lix.py
@@ -0,0 +1,49 @@
+import pandas as pd
+
+def lixdata():
+
+    df_first_clean = pd.read_pickle('../notebooks/pickled_data/data_first_clean.pkl')
+
+
+    # Copy main dataframe (first clean - we don't want lemmatization here!):
+    df_lix = df_first_clean.copy()
+    df_lix = df_lix.drop(columns = ['speech', 'first_clean_tokenized'])
+
+
+    # WORD COUNT:
+    # Method to calculate total words in each speach:
+    def word_count(speech):
+        return sum(1 for word in speech.split(' '))
+
+    # Get total words in each speech and add result to DataFrame:
+    df_lix['word_count'] = df_lix.first_clean.apply(word_count)
+
+
+    # COUNT OF PERIODS:
+    # Method to calculate total number of periods (period or colon) in each speach:
+    def period_count(speech):
+        result = speech.count('.') + speech.count(':')
+        return result
+
+    # Get total number of periods in each speech and add result to DataFrame:
+    df_lix['period_count'] = df_lix.first_clean.apply(period_count)
+
+
+    # COUNT OF LONG WORDS:
+    # Method to long words (< 6 characters) in each speach:
+    def long_word_count(speech):
+        count = 0
+        for word in speech.split(' '):
+            if len(word) > 6:
+                count += 1
+        return count
+
+    # Get total number of long words in each speech and add result to DataFrame:
+    df_lix['long_words_count'] = df_lix.first_clean.apply(long_word_count)
+
+
+    # CALCULATE LIX:
+    # Calculates LIX for each speech and adds result to DataFrame:
+    df_lix['lix'] = (df_lix.word_count/df_lix.period_count) + ((df_lix.long_words_count*100) / df_lix.word_count)
+
+    return df_lix
--- flask_app/main.py
+++ flask_app/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from fetchSpeech import getspeech
 import pandas as pd
 from sentiment import sentiment
+from lix import lixdata
+
 
 app = Flask(__name__)
 
@@ -25,6 +27,32 @@ def getSentiment():
     df = sentiment()
     df2 = df.drop(columns = ['speech', 'first_clean', 'first_clean_tokenized'])
     return df2.to_html()
+
+
+@app.route('/lix')
+def getLix():
+    df = lixdata()
+    df2 = df[["year", "lix"]]
+    data = df2.to_records(index=False)
+    print(data)
+
+    labels= [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template("lix_chart.html", labels= labels, values = values)
+
+@app.route('/obamatrump')
+def obamaVsTrump():
+    df = lixdata()
+    mask_obama = (df['President'] == 'Barack Obama')
+    mask_trump = (df['President'] == 'Donald Trump')
+    avg_lix_obama = df[mask_obama]['lix'].mean()
+    avg_lix_trump = df[mask_trump]['lix'].mean()
+    data = [("obama", avg_lix_obama), ("Trump",avg_lix_trump )]
+    print(data)
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+    return render_template("obamaVsTrump.html", labels= labels, values = values)
+
 
 
 
